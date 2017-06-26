5acd1d5d272863209866323af56b9c3b732b501b
jobiols <jorge.obiols@gmail.com>
2017-06-26 18:34:24 -0300

    commit inicial

---
+++ varazdin_default/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'varazdin_default',
+    'version' : '10.0.0.1',
+    'summary': 'default module for varazdin',
+    'sequence': 30,
+    'description': """
+Varazdin
+========
+    """,
+    'category': 'Default',
+    'website': 'http://www.jeosoft.com',
+    'images' : [],
+    'depends' : ['stock'],
+    'data': [  ],
+    'demo': [  ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
